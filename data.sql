cdb33ff49ae363d74ae5e764b24d7bc8c289f994
AnthonyPerfors <145234145+AnthonyPerfors@users.noreply.github.com>
2023-12-15 13:30:12 -0600

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,17 @@
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Bob', 'Smith', 'bobSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Tony', 'Smith', 'tSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Jim', 'Smith', 'jSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Larry', 'Smith', 'lSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Harry', 'Smith', 'hSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Dave', 'Smith', 'dSmith@yahoo.com');
+insert into customer (customer_first_name, customer_last_name, customer_email) values ('Joe', 'Smith', 'joeSmith@yahoo.com');
+
+insert into pet_store (pet_store_name, pet_store_address,pet_store_city,pet_store_state,pet_store_zip,pet_store_phone) values ('Pets', '742 washway','flipper','IL','77889','1234567890');
+insert into pet_store (pet_store_name, pet_store_address,pet_store_city,pet_store_state,pet_store_zip,pet_store_phone) values ('Pat', '123 washway','gainz','TX','44556','7410258963');
+insert into pet_store (pet_store_name, pet_store_address,pet_store_city,pet_store_state,pet_store_zip,pet_store_phone) values ('Dog', '456 washway','paw','OH','11223','2584631987');
+
+insert into employee (pet_store_id,employee_first_name,employee_last_name,employee_phone,employee_job_title) values ('1','Tom','Carter','8521023654','Manager');
+insert into employee (pet_store_id,employee_first_name,employee_last_name,employee_phone,employee_job_title) values ('2','Dan','Carter','5431258750','Sales');
+insert into employee (pet_store_id,employee_first_name,employee_last_name,employee_phone,employee_job_title) values ('3','Diana','Carter','6352148752','Advertising');
+insert into employee (pet_store_id,employee_first_name,employee_last_name,employee_phone,employee_job_title) values ('2','Beth','Carter','9654120355','Cashier');
+insert into employee (pet_store_id,employee_first_name,employee_last_name,employee_phone,employee_job_title) values ('1','Josh','Carter','8844563210','Cashier');
